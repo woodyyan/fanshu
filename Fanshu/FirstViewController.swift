b65644eb4a100abc3c15aefac8ecbd4673691c2a
WoodyYan <colorguitar@hotmail.com>
2018-03-16 14:46:03 +0800

    Initial Commit

---
+++ Fanshu/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fanshu
+//
+//  Created by Songbai Yan on 16/03/2018.
+//  Copyright © 2018 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
