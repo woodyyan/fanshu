2b8f39fe4fa6e6eda4da65cf186cb6154b14c983
Kyle He <kyle306@gmail.com>
2018-04-15 23:14:40 +0800

    Wei - Add three pages

--- Fanshu/DiscoveryViewController.swift
+++ Fanshu/DiscoveryViewController.swift
@@ -12,6 +12,15 @@ class DiscoveryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let recomendViewController = self.storyboard?.instantiateViewController(withIdentifier: "recommend")
+        let issuesViewController = self.storyboard?.instantiateViewController(withIdentifier: "issues")
+        let specialViewController = self.storyboard?.instantiateViewController(withIdentifier: "special")
+        
+        self.view.addSubview((recomendViewController?.view)!)
+        self.view.addSubview((issuesViewController?.view)!)
+        self.view.addSubview((specialViewController?.view)!)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
